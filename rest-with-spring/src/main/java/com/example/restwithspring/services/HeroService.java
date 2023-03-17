66945772ba3bb4bff846042be0196829709740db
Yan Cunha <yan.cunha@bitka.com.br>
2023-03-17 16:59:16 -0300

    Preenche funções do service
    Aplicando este commit o service passará a utilizar o repository, e assim buscando as informações do banco de dados.

--- rest-with-spring/src/main/java/com/example/restwithspring/services/HeroService.java
+++ rest-with-spring/src/main/java/com/example/restwithspring/services/HeroService.java
@@ -1,30 +1,45 @@
 package com.example.restwithspring.services;
+
 import com.example.restwithspring.model.Hero;
+import com.example.restwithspring.repositories.IHeroRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 
 @Service
 public class HeroService {
 
+    private static IHeroRepository heroRepository;
+
+    @Autowired
+    public HeroService(HeroRepository heroRepository) {
+        this.heroRepository = heroRepository;
+    }
+
+    @Transactional
     public static Hero save(Hero hero) {
-        return new Hero();
+        return  heroRepository.save(hero);
     }
 
     public boolean hasHero(String name) {
-        return true;
+        return heroRepository.existsByName(name);
     }
 
     public List<Hero> findAll() {
-        return new ArrayList<>();
+        return heroRepository.findAll();
     }
 
     public Optional<Hero> findById(int id) {
-        return null;
+        return heroRepository.findById(id);
     }
 
+    @Transactional
     public void delete(Hero hero) {
+        heroRepository.delete(hero);
     }
 }
+
